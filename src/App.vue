2da65ca4bdba55fbab951ca63028bdbb08def7ae
zhangze-github <1183460943@qq.com>
2022-12-28 08:23:19 +0000

    iniit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <gird></gird>
+  </div>
+</template>
+
+<script setup>
+    import VueGridLayout from 'vue-grid-layout';
+    import gird from './gird.vue';
+</script>
+
+<style scoped>
+
+</style>
